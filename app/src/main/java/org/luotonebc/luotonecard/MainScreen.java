271556fda7bf57edc69639781bb4f5c281f8cf08
JuusoKorvola <juuso.korvola@gmail.com>
2019-03-12 22:54:27 +0200

    Added Main Screen rough layout with buttons, but no functionality. Added a bunch of string values to strings.xml for button names

---
+++ app/src/main/java/org/luotonebc/luotonecard/MainScreen.java
@@ -0,0 +1,22 @@
+package org.luotonebc.luotonecard;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
