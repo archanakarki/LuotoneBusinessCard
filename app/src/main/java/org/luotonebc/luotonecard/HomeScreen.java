9afe0148a465b50d5648c0f4a5dbeb316dbdf55c
leizyy <emilia.hk.oksanen@gmail.com>
2019-03-18 23:37:59 +0200

    Added the actual login screen

--- app/src/main/java/org/luotonebc/luotonecard/HomeScreen.java
+++ app/src/main/java/org/luotonebc/luotonecard/HomeScreen.java
@@ -1,30 +1,30 @@
 package org.luotonebc.luotonecard;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class LoginScreen extends AppCompatActivity {
+public class HomeScreen extends AppCompatActivity {
     private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_screen);
+        setContentView(R.layout.activity_home_screen);
 
         button = findViewById(R.id.login);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openMainScreen();
+                openLoginScreen();
             }
         });
     }
 
-    public void openMainScreen() {
-        Intent intent = new Intent (this, MainScreen.class);
+    public void openLoginScreen() {
+        Intent intent = new Intent(this, LoginScreen.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/org/luotonebc/luotonecard/SplashScreen.java
+++ app/src/main/java/org/luotonebc/luotonecard/SplashScreen.java
@@ -15,7 +15,7 @@ public class SplashScreen extends AppCompatActivity {
             public void run() {
                 try {
                     sleep(3000);
-                    Intent intent = new Intent (getApplicationContext(), LoginScreen.class);
+                    Intent intent = new Intent (getApplicationContext(), HomeScreen.class);
                     startActivity(intent);
                     finish();
                 } catch (InterruptedException e) {
